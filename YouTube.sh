72646bd036db8ed22cb08853b3db7023ef3a2321
Olpas9 <66534811+Olpas9@users.noreply.github.com>
2020-07-13 03:56:40 +0300

    Update YouTube.sh

--- YouTube.sh
+++ YouTube.sh
@@ -3,36 +3,36 @@
 # Author: Olpas9
 # Instagram: htttps://www.instagram.com/olpas9
 
-echo -e "\e[032m" updating default packages \n"
+echo -e "\e[032m" "updating default packages \n"
   apt update && apt upgrade
 
-echo -e "\e[032m" Allow storage permission \n"
+echo -e "\e[032m" "Allow storage permission \n"
   termux-setup-storage
 sleep 3
 
-echo -e "\e[032m" installing git \n"
+echo -e "\e[032m" "installing git \n"
  pkg install git -y
 
-echo -e "\e[035m"installing youtube-dl \n"
+echo -e "\e[035m" "installing youtube-dl \n"
  pip install youtube-dl
 
-echo -e "\e[032m" making directory of youtube \n"
- mkidir -/storage/shared/YouTube
+echo -e "\e[032m" "making directory of youtube \n"
+ mkdir ~/storage/shared/YouTube
 
-echo -e "\e[032m" creating youtube-dl folder \n"
- mkidir -/.config/youtube-dl
+echo -e "\e[032m" "creating youtube-dl folder \n"
+ mkdir ~/.config/youtube-dl
 
-echo -e "\e[032m" creating bin folder \n"
- mkidir -/bin
+echo -e "\e[032m" "creating bin folder \n"
+ mkdir ~/bin
 
-echo -e "\e[034m" termux-url-opener creation \n"
+echo -e "\e[034m" "termux-url-opener creation \n"
 
- mv termux-url-opner -/bin/
+ mv termux-url-opner ~/bin/
 echo -e "\n"
-echo -e "\e[032m"  Done \n"
-echo -e "\e[035m" Now you have done So you can visit YouTube videos and choose any video you like, then click share bottom and choose to share into termux ,Choose quality of video then enter after that u will find YouTube videos in your internal storage \n"
-echo -e "\e[034m" Follow me Instagram https://www.instagram.com/olpas9 \n"
-echo -e "\e[032m" HACKER FOR LIFE ☠️☠️  \n"
+echo -e "\e[032m" " Done \n"
+echo -e "\e[035m" " Now you have done So you can visit YouTube videos and choose any video you like, then click share bottom and choose to share into termux ,Choose quality of video then enter after that u will find YouTube videos in your internal storage \n"
+echo -e "\e[034m" " Follow me Instagram https://www.instagram.com/olpas9 \n"
+echo -e "\e[032m" " HACKER FOR LIFE ☠️☠️  \n"
 
 
 
